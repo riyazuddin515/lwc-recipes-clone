94ef33a22123b8b9310171b1f1ca001597332090
riyaz <riyazuddin515@gmail.com>
2023-05-23 21:52:55 +0530

    Completed helloExpression Component

---
+++ force-app/main/default/lwc/helloExpression/helloExpression.js
@@ -0,0 +1,17 @@
+import { LightningElement } from 'lwc';
+
+export default class HelloExpression extends LightningElement {
+    firstName = "";
+    lastName = "";
+
+    handleChange(event) {
+        if (event.target.name === 'firstName') {
+            this.firstName = event.target.value;
+        } else {
+            this.lastName = event.target.value;
+        }
+    }
+    get upperCaseFullName() {
+        return `${this.firstName} ${this.lastName}`.toUpperCase();
+    }
+}
